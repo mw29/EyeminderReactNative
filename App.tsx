ebac21b77f4fe347b577b3b9f49848f94d09e9ec
Madeline Watts <madeline.chloe429@gmail.com>
2025-05-24 16:21:37 +0100

    set status and footer color

--- App.tsx
+++ App.tsx
@@ -5,34 +5,42 @@ import HomeScreen from './src/screens/HomeScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import {RootStackParamList} from './src/types/NavigationTypes';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, View} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer
-        theme={{
-          ...DefaultTheme,
-          colors: {
-            ...DefaultTheme.colors,
-            background: 'white',
-          },
-        }}
-        >
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
+      <View style={styles.container}>
+        <SafeAreaView style={{flex: 1}}>
+          <NavigationContainer
+            theme={{
+              ...DefaultTheme,
+              colors: {
+                ...DefaultTheme.colors,
+                background: 'white',
+              },
             }}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            {/* Profile is not needed for this project yet */}
-            {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
+      </View>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
