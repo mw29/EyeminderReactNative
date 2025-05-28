6166898e7cc7e306a5cfe0e5e921c537745b7dbd
Madeline Watts <madeline.chloe429@gmail.com>
2025-05-28 11:47:48 +0200

    add layout config for HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {HomeScreenNavigationProp} from '../types/NavigationTypes';
+import DayCounter from '../components/DayCounter';
 
 type Props = {
   navigation: HomeScreenNavigationProp;
@@ -8,14 +9,37 @@ type Props = {
 
 function HomeScreen({navigation}: Props): React.JSX.Element {
   return (
-    <View style={{flex: 1,padding: 5, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>HomePage</Text>
-      <Button
-        title="Go to the profile screen"
-        onPress={() => navigation.navigate('Profile', {name: "Jane"})}
-      />
+    <View style={{flex: 1}}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Eyeminder</Text>
+      </View>
+      <View style={styles.centerContainer}>
+        <DayCounter/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Text>BOTTOM</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    alignItems: 'center',
+  },
+  headerText: {
+    fontWeight: 500,
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerContainer: {
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen;
